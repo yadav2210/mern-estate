037f9c452eade4e6656cfb27f0331a346007ac3b
Shreya Jagatnarayan Yadav <yadavshreya2131@gmail.com>
2025-07-18 17:05:46 +0530

    firstcommit

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+const App = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">Hello, World!</h1>
+    </div>
+  );
+}
+export default App;
